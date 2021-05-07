7afd950db8ab75b5d0aa7c4ae5562cfd8fe86c4a
Fernando Souza <fernandosouzaconceicao@gmail.com>
2021-05-07 11:50:24 -0300

    Either.kt

---
+++ domain/src/main/java/br/com/fernandodeveloper/domain/utils/Either.kt
@@ -0,0 +1,13 @@
+package br.com.fernandodeveloper.domain.utils
+
+sealed class Either<out A, out B> {
+
+    data class Success<out A>(
+        val data: A
+    ) : Either<A, Nothing>()
+
+    data class Failure<out B>(
+        val cause: B
+    ) : Either<Nothing, B>()
+}
+
